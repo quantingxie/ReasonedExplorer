21ae30bf8250d425189c0c5011f18ce5b45d1551
quantingxie <xiequanting@icloud.com>
2023-08-30 22:36:23 -0400

    2

--- robot_src/rotate_pic.py
+++ robot_src/rotate_pic.py
@@ -10,16 +10,18 @@ cmd = sdk.HighCmd()
 state = sdk.HighState()
 udp.InitCmdData(cmd)
 
-
 def capture_images_by_rotate(n: int, range_of_motion=50) -> list:
     captured_images = []
 
-    # Calculate min_angle and max_angle based on range_of_motion
-    min_angle = -range_of_motion / 2
-    max_angle = range_of_motion / 2
+    # Convert range_of_motion to radians since the calculations are in degrees
+    range_of_motion_radians = math.radians(range_of_motion)
 
-    # Calculate the angle increment
-    angle_increment = math.radians(range_of_motion) / n
+    # Calculate min_angle and max_angle based on range_of_motion in radians
+    min_angle = -range_of_motion_radians / 2
+    max_angle = range_of_motion_radians / 2
+
+    # Calculate the angle increment in radians
+    angle_increment = range_of_motion_radians / n
 
     # Capture images while rotating to the left (from 0 to min_angle)
     for i in range(0, n//2):
@@ -30,7 +32,7 @@ def capture_images_by_rotate(n: int, range_of_motion=50) -> list:
 
         udp.SetSend(cmd)
         udp.Send()
-        time.sleep(100)
+        time.sleep(1)  # Changed from 100 to 1 since 100 seconds might be too long
 
         angle_in_degrees = math.degrees(yaw_angle)
         image = capture_image_at_angle(angle_in_degrees)
@@ -52,7 +54,7 @@ def capture_images_by_rotate(n: int, range_of_motion=50) -> list:
 
         udp.SetSend(cmd)
         udp.Send()
-        time.sleep(100)
+        time.sleep(1)  # Changed from 100 to 1
 
         angle_in_degrees = math.degrees(yaw_angle)
         image = capture_image_at_angle(angle_in_degrees)
@@ -76,5 +78,3 @@ reached_goal = False
 for image in captured_images:
     description, found = VLM_query(image)
     curr_nodes_data.append((description, found))
-
-
