e0a5bdd4f4fe941c63d250fa8041301397f52d5e
quantingxie <xiequanting@icloud.com>
2023-08-30 22:41:41 -0400

    changed rotate pic to just from left to right

--- robot_src/rotate_pic.py
+++ robot_src/rotate_pic.py
@@ -13,18 +13,17 @@ udp.InitCmdData(cmd)
 def capture_images_by_rotate(n: int, range_of_motion=50) -> list:
     captured_images = []
 
-    # Convert range_of_motion to radians since the calculations are in degrees
+    # Convert range_of_motion to radians
     range_of_motion_radians = math.radians(range_of_motion)
 
     # Calculate min_angle and max_angle based on range_of_motion in radians
     min_angle = -range_of_motion_radians / 2
-    max_angle = range_of_motion_radians / 2
-
+    
     # Calculate the angle increment in radians
     angle_increment = range_of_motion_radians / n
 
-    # Capture images while rotating to the left (from 0 to min_angle)
-    for i in range(0, n//2):
+    # Capture images while rotating from the left to right (from min_angle to max_angle)
+    for i in range(n):
         yaw_angle = min_angle + i * angle_increment
 
         cmd.euler = [0, 0, yaw_angle]
@@ -32,29 +31,7 @@ def capture_images_by_rotate(n: int, range_of_motion=50) -> list:
 
         udp.SetSend(cmd)
         udp.Send()
-        time.sleep(1)  # Changed from 100 to 1 since 100 seconds might be too long
-
-        angle_in_degrees = math.degrees(yaw_angle)
-        image = capture_image_at_angle(angle_in_degrees)
-        if image is not None:
-            captured_images.append(image)
-
-    # Reset to 0 before moving to the right
-    cmd.euler = [0, 0, 0]
-    udp.SetSend(cmd)
-    udp.Send()
-    time.sleep(1)
-
-    # Capture images while rotating to the right (from 0 to max_angle)
-    for i in range(n//2, n):
-        yaw_angle = i * angle_increment
-
-        cmd.euler = [0, 0, yaw_angle]
-        cmd.mode = 1
-
-        udp.SetSend(cmd)
-        udp.Send()
-        time.sleep(1)  # Changed from 100 to 1
+        time.sleep(1000)
 
         angle_in_degrees = math.degrees(yaw_angle)
         image = capture_image_at_angle(angle_in_degrees)
