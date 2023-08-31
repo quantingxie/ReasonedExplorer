58d6c9d3271e0b6a59a70b7282f590e4aed5b5fd
quantingxie <xiequanting@icloud.com>
2023-08-30 22:21:19 -0400

    add rotation standalone test

---
+++ robot_src/test_rotation.py
@@ -0,0 +1,17 @@
+HIGHLEVEL = 0xee
+import robot_interface as sdk
+
+
+yaw_angle = 10
+
+
+udp = sdk.UDP(HIGHLEVEL, 8080, "192.168.123.161", 8082)
+cmd = sdk.HighCmd()
+state = sdk.HighState()
+udp.InitCmdData(cmd)
+
+cmd.euler = [0, 0, yaw_angle]
+cmd.mode = 1
+
+udp.SetSend(cmd)
+udp.Send()
