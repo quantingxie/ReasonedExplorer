3d9d9e1670881ab5f2dc78e0e63a30390dc1f93a
tyz <zty181030@gmail.com>
2023-09-02 15:26:49 -0400

    imu

--- robot_src/test_IMU.py
+++ robot_src/test_IMU.py
@@ -1,5 +1,5 @@
 import serial
-
+import time
 
 def adjust_heading(heading):
     if 0 <= heading <= 180:
@@ -8,18 +8,20 @@ def adjust_heading(heading):
         return -(heading - 360)
 
 
-def read_from_serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1):
+def read_from_serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
         while True:
             line = ser.readline()  # read a line terminated with a newline (\n)
             try:
                 decoded_line = line.decode('utf-8').strip()
-                if decoded_line.startswith('$PRDID'):
+                # print(decoded_line)
+                if decoded_line.startswith('$HCHDG'):
                     # Split the line by commas
                     parts = decoded_line.split(',')
                     # Heading (relative to true north) is the third value in the list
-                    heading = float(parts[3])
-                    heading = adjust_heading(heading)
+                    heading = float(parts[1])
+                    # print(decoded_line)
+                    print("current time: "+time.strftime("%H:%M:%S", time.localtime()))
                     print(heading)
             except UnicodeDecodeError:
                 # Silently ignore decoding errors
