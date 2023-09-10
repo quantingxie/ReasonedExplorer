23936a79cb238f9880db6c1783d3aea3ce8b3e4b
Tianyi Zhang <zty181030@gmail.com>
2023-09-10 16:59:47 -0400

    googlemap_cotrl

---
+++ src/exploration_google_map.py
@@ -0,0 +1,337 @@
+import math
+import time
+import airsim
+import networkx as nx
+import cv2
+from typing import List, Tuple
+from LLM_functions import LLM_abstractor, LLM_rephraser, LLM_checker
+from VLM import VLM_query
+from utils import *
+from simulator_utils import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import logging
+import keyboard
+from LLM_functions import LLM_evaluator
+import robot_interface as sdk
+
+MY_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY'
+
+
+def capture_image_at_angle(angle):
+    camera = cv2.VideoCapture(1)
+    if not camera.isOpened():
+        print("Error: Could not open camera.")
+        return
+    
+    ret, frame = camera.read()
+    frame = cv2.flip(frame, 0)
+    frame = cv2.flip(frame, 1)
+
+    cv2.imwrite(f"{angle}_degrees.jpg", frame)
+
+    if not ret:
+        print(f"Error: Couldn't capture image at angle {angle}.")
+        return None
+
+    return f"{angle}_degrees.jpg"
+
+class Node:
+    def __init__(self, Q, id, description, gps, yaw_angle):
+        self.id = id  # Unique identifier for the node
+        self.Q = Q
+        self.score = 0.0
+        self.description = description  # Scene description from VLM or LLM_rephraser
+        self.gps = gps  # GPS data
+        self.yaw_angle = yaw_angle  # Yaw angle
+        self.visited = False  # Whether the node has been visited or not
+
+
+class Exploration:
+    next_node_id = 0
+    def __init__(self, mcts, x, k, n, fov, rom, goal, model):
+        self.x = x
+        self.k = k
+        self.n = n
+        self.fov = fov
+        self.rom = rom
+        self.graph = nx.Graph()
+        self.iteration_count = 0
+        self.goal = goal
+        self.model = model
+        self.state = {}
+        self.nodes = []
+        self.H = [[], [], []]  # Hierarchical abstract
+        self.mcts = mcts
+        self.frontier_buffer = []
+        self.Q_buffer = {}  # Dictionary to store Q-values for nodes
+        logging.basicConfig(filename='exploration.log', level=logging.INFO, format='%(message)s')
+        sys.stdout = Logger("my_output2.log")
+        # Initialization for robot control
+        self.current_node = None  # Track the current position of the agent
+        # initialize simulator
+        # Initialization for robot control
+        self.current_node = None  # Track the current position of the agent
+        self.HIGHLEVEL = 0xee
+        self.udp = sdk.UDP(self.HIGHLEVEL, 8080, "192.168.123.161", 8082)
+        self.cmd = sdk.HighCmd()
+        self.state = sdk.HighState()
+        self.udp.InitCmdData(self.cmd)
+
+
+    def adaptive_step_size(self, current_score, min_step, max_step):
+        normalized_score = current_score / 5
+        return min_step + (1 - normalized_score) * (max_step - min_step)
+
+
+    def draw_path(self, filename_prefix="path_map.html"):
+        # Assuming each node has gps_data as (lat, lon)
+        lat0, lon0 = self.nodes[0].gps
+
+        gmap_graph = gmplot.GoogleMapPlotter(lat0, lon0, 19)
+        gmap_graph.apikey = MY_API_KEY
+        gmap_graph.map_type = "satellite"
+
+        # Add nodes to the map
+        for node in self.graph.nodes:
+            color = 'green' if node.visited else 'red'
+            gmap_graph.circle(node.gps[0], node.gps[1], 2, color=color)
+            
+            # Draw yaw as an arrow (using the node's position and yaw_angle)
+            arrow_length = 0.0001  # Adjust this for the scale of your map
+            dx = arrow_length * math.sin(math.radians(node.yaw_angle))  # sin for East (from yaw angle)
+            dy = arrow_length * math.cos(math.radians(node.yaw_angle))  # cos for North (from yaw angle)
+            
+            # Calculate end point for the arrow
+            lat_end = node.gps[0] + dy
+            lon_end = node.gps[1] + dx
+            
+            # Draw arrow as a line segment
+            gmap_graph.plot([node.gps[0], lat_end], [node.gps[1], lon_end], 'red', edge_width=2.5)
+
+            # Annotate score if present
+            if hasattr(node, 'score') and node.score > 0:
+                gmap_graph.marker(node.gps[0], node.gps[1], title=f"{node.score:.2f}")
+
+        # Add connections (edges) between nodes
+        for edge in self.graph.edges:
+            node1, node2 = edge
+            gmap_graph.plot([node1.gps[0], node2.gps[0]], [node1.gps[1], node2.gps[1]], 'blue', edge_width=2.5)
+
+        # Save the map to an HTML file
+        filename = f"{filename_prefix}_step_{self.iteration_count}.html"
+        gmap_graph.draw(filename)
+
+    """Methods for graph"""
+    def add_node_to_graph(self, Q,  description, gps, yaw_angle):
+        node = Node(Q, Exploration.next_node_id, description, gps, yaw_angle)
+        Exploration.next_node_id += 1  # Increment the ID for the next node
+        self.graph.add_node(node)
+        node.visited = False
+        return node
+        
+    def connect_nodes(self, node1, node2):
+        weight = self.compute_distance(node1.gps, node2.gps)
+        self.graph.add_edge(node1, node2, weight=weight)
+
+    def compute_distance(self, gps1, gps2):
+        # Compute distance between two GPS points (you can use the provided conversion functions)
+        # This is just a placeholder; replace it with an appropriate function
+        lat_diff = gps2[0] - gps1[0]
+        lon_diff = gps2[1] - gps1[1]
+        lat_distance = lat_to_meters(lat_diff)
+        lon_distance = lon_to_meters(lon_diff, gps1[0])
+        return math.sqrt(lat_distance**2 + lon_distance**2)
+    
+
+    def explore(self) -> None:
+        # Initialize current_node at the beginning of exploration
+        step_counter = 0
+        initial_gps = 40.444612, -79.947429
+        initial_yaw = -90
+        self.current_node = self.add_node_to_graph(0, "", initial_gps, initial_yaw)
+        self.current_node.visited = True
+
+        while True:
+
+            current_gps = self.current_node.gps
+            current_yaw = self.current_node.yaw_angle
+            self.current_node.visited = True
+            current_score = self.current_node.Q
+            step_size = self.adaptive_step_size(current_score, 3e-5, 7e-5)
+            print(step_size)
+            print("GLOBAL STEP: ", step_counter)
+            print("Current_Pos", current_gps)
+            print("Current_yaw", current_yaw)
+            captured_images = self.capture_images_by_rotate(self.n, self.rom)
+            curr_nodes_data = []
+            for image in captured_images:
+                description = VLM_query(image)
+                curr_nodes_data.append((description))
+
+
+            step_counter += 1
+            # Calculate yaw angles and GPS coordinates for each captured image
+            nodes = []
+            for i, description in enumerate(curr_nodes_data):
+                direction = i * (self.rom / (self.n - 1))
+
+                # Incorporating the actual yaw into the calculated yaw angle
+                yaw_angle = current_yaw - direction + self.rom/2  # Adjusting for center of the FOV
+
+                # print("Yaw_angle", yaw_angle)
+                dx = step_size * math.cos(math.radians(yaw_angle))
+                dy = step_size * math.sin(math.radians(yaw_angle))
+
+                newlat = current_gps[0] + dy
+                newlon = current_gps[1] - dx
+              
+                node = self.add_node_to_graph(0, description, (newlat, newlon), yaw_angle)
+                print("node expanded in ", node.gps, "for angle", node.yaw_angle, "Description", node.description, "\n")
+                self.connect_nodes(self.current_node, node)
+                nodes.append(node)
+
+       
+            print("Running MCTS")
+            descriptions = [node.description for node in nodes]
+            # self.mcts.run_mcts(self.k, descriptions) # MCTS real
+            found = False
+
+            #Baseline 1
+            # naiveLLM_start_time = time.time()
+            # for node in nodes:
+            #     print(self.goal)
+            #     print("Node Description", node.description)
+            #     node.Q = LLM_evaluator(node.description, goal=self.goal, model="gpt-4")
+            #     print("NodeQ:", node.Q)
+            #     check = LLM_checker(node.description, self.goal, model="gpt-4")
+
+            #     print("Goal Found? ", check)
+            #     if check.strip() == "Yes":
+            #         print("!!! Found GOAL !!!")
+            #         self.client.hoverAsync().join()
+            #         found = True
+            #         break
+            # if found:
+            #     break
+            # naiveLLM_end_time = time.time()
+            # print("NaiveLLM thinking time: ", naiveLLM_end_time - naiveLLM_start_time, "seconds")
+
+            # MCTS
+            mcts_start_time = time.time()
+            self.mcts.run_mcts(descriptions)
+            mcts_end_time = time.time()
+            print("MCTS thinking time: ", mcts_end_time - mcts_start_time, "seconds")
+
+            print("User-Instructions: ", self.goal)
+            for node in nodes:
+                node.Q = self.mcts.Q.get(str(node.description), 0)
+                
+                check = LLM_checker(node.description, self.goal, model="gpt-4")
+                print("Goal Found? ", check)
+                if check.strip() == "Yes":
+                    print("!!! Found GOAL !!!")
+                    self.client.hoverAsync().join()
+                    found = True
+                    break
+            if found:
+                break
+
+
+            action_start_time = time.time()
+            self.action()
+            action_end_time = time.time()
+            print("Action time: ", action_end_time - action_start_time, "seconds")
+
+
+    """Methods for robot movement"""
+    def action(self) -> None:
+        """Chooses and executes an action based on the Q-values."""
+        # Compute distances to frontier nodes using Euclidean distance
+
+        nodes_gps_list = [node.gps for node in self.graph.nodes if not node.visited] # changes
+
+        d_values = compute_euclidean_distances_from_current(self.current_node.gps, nodes_gps_list)
+        print("d values", d_values)
+
+        unvisited_nodes = [node for node in self.graph.nodes if not node.visited] # changes
+ 
+        scores = [(node, node.Q -  0*d_values[nodes_gps_list.index(node.gps)]) for node in unvisited_nodes] # changes unvisited_nodes
+        print("SCORES", scores)
+        self.chosen_node = max(scores, key=lambda x: x[1])[0]
+        for node, score in scores:
+            node.score = score
+        self.chosen_node.visited = True
+
+        self.draw_path()
+
+        print("Press 's' to start the action and 'e' to end the action.")
+
+        # Wait for the 's' key to be pressed to start the action
+        keyboard.wait('s')
+        print("Starting the action...") 
+
+        keyboard.wait('e')
+        print("Action completed.")
+
+        self.current_node = self.chosen_node
+        self.current_node.visited = True
+
+
+    def capture_images_by_rotate(self, n: int, range_of_motion=20) -> list:
+        captured_images = []
+
+        # Convert range_of_motion to radians
+        range_of_motion_radians = math.radians(range_of_motion)
+
+        # Calculate min_angle based on range_of_motion in radians
+        min_angle = -range_of_motion_radians / 2
+
+        # Calculate the angle increment in radians
+        angle_increment = range_of_motion_radians / n
+
+        # Initialize motion time and yaw angle
+        motiontime = 0
+        yaw_angle = min_angle
+
+        while motiontime < n+1:
+            time.sleep(0.005)
+            motiontime += 1
+
+            # Receive current state (Not necessary for your function, but kept for compatibility)
+            self.udp.Recv()
+            self.udp.GetRecv(self.state)
+
+            # Set mode and euler angles for the robot
+            self.cmd.mode = 1
+            self.cmd.euler = [0, 0, yaw_angle]
+
+            # Send the udp command
+            self.udp.SetSend(self.cmd)
+            self.udp.Send()
+
+            # Capture image and append it if it's not None
+            angle_in_degrees = math.degrees(yaw_angle)
+            image = capture_image_at_angle(angle_in_degrees)
+            if image is not None:
+                captured_images.append(image)
+
+            # Update the yaw_angle for the next iteration
+            yaw_angle += angle_increment
+
+        return captured_images
+    
+import sys
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        pass    
---
+++ src/simulate_google_map.py
@@ -0,0 +1,36 @@
+# from MCTS_async import MCTS
+from MCTS_simplified import MCTS
+from exploration_google_map import Exploration
+import openai
+import os
+if __name__ == '__main__':
+
+    
+    openai.api_key = "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"
+    MODEL = "gpt-3.5-turbo-16k"
+    os.environ['OPENAI_API_KEY'] = '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'
+    print(os.getenv("OPENAI_API_KEY"))
+    ETA = 1 # Scaling factor for exploration
+    GAMMA = 3 # N(S_t+1)^gamma
+    X = 5  # X nodes to abstract
+    K = 10  # How many iterations to run MCTS at each step
+    N = 3 # Action space and tree width
+    L = 1  # Tree length
+    fov = 60 # Field of View
+    rom = 60 # Range of motion
+    explorer_instance = None  # Declare this outside the try block so it can be accessed in the finally block
+
+    try:
+
+        goal = "Find a red car in the parking lot"
+        # Initialize MCTS
+        mcts_instance = MCTS(N, L, goal, MODEL)
+        print("MCTS Instance established")
+        # Initialize Exploration with the MCTS instance
+        explorer_instance = Exploration(mcts_instance, X, K, N, fov, rom, goal, MODEL)
+        print("Explorer")
+        # Run the exploration process
+        explorer_instance.explore()
+
+    except KeyboardInterrupt:
+        print("\nReceived a KeyboardInterrupt! Stopping the exploration process.")
