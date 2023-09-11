a12deb6d886e8e95f6ec4e1f73d1210aa00d7763
Tianyi Zhang <zty181030@gmail.com>
2023-09-11 09:21:28 -0400

    test

--- src/exploration_google_map.py
+++ src/exploration_google_map.py
@@ -281,28 +281,30 @@ class Exploration:
             if(motiontime > 1000 and motiontime < 2000):
                 self.cmd.mode = 1
                 self.cmd.euler = [0, 0, math.radians(-25)]
-                print("TESTTTS")
-                print("motiontime", motiontime)
+
+            if(motiontime > 2000 and motiontime < 2002):
                 # Capture image 
                 image = capture_image_at_angle(-60, self.step_counter)
                 if image is not None:
                     captured_images.append(image)
                 
-            if(motiontime > 2000 and motiontime < 2200):
+            if(motiontime > 2002 and motiontime < 2201):
                 print("Test 2")
                 self.cmd.mode = 1
                 self.cmd.euler = [0, 0, 0]
 
-            if(motiontime > 2200 and motiontime < 3200):
+            if(motiontime > 2201 and motiontime < 3201):
                 self.cmd.mode = 2
                 self.cmd.yawSpeed = 0.610865
 
+
+            if(motiontime > 3201 and motiontime < 3203):
                 # Capture image
                 image = capture_image_at_angle(0, self.step_counter)
                 if image is not None:
                     captured_images.append(image)
 
-            if(motiontime > 3200 and motiontime < 4200):
+            if(motiontime > 3203 and motiontime < 4200):
                 self.cmd.mode = 2
                 self.cmd.yawSpeed = 0.610865
 
@@ -311,11 +313,12 @@ class Exploration:
                 self.cmd.euler = [0, 0, math.radians(25)]
 
                 # Capture image
+            if(motiontime > 5200 and motiontime < 5201):
                 image = capture_image_at_angle(0, self.step_counter)
                 if image is not None:
                     captured_images.append(image)
 
-            if(motiontime > 5200 and motiontime < 5400):
+            if(motiontime > 5201 and motiontime < 5400):
                 self.cmd.mode = 1
                 self.cmd.euler = [0, 0, 0]
 
